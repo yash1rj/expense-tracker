725e924c2a11c390bb8dbc3b3b19b99ef4a1dfce
Yash Raj <yashrj130@gmail.com>
2021-05-23 16:31:37 +0530

    Singular expense item

---
+++ src/components/ExpenseItems/ExpenseItem/ExpenseItem.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import classes from './ExpenseItem.module.css';
+import ExpenseDate from './ExpenseDate';
+import Card from '../../Card/Card';
+
+const ExpenseItem = props => {
+
+    return (
+        <Card className={classes.expenseItem}>
+            <ExpenseDate date={props.date} />
+            <div className={classes.expenseItem__description}>
+                <h2>{props.title}</h2>
+                <div className={classes.expenseItem__price}>${props.amount}</div>
+            </div>
+        </Card>
+    );
+};
+
+export default ExpenseItem;
