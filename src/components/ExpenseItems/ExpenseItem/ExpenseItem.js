0c043ed787ff40ea52103c0d87a2d0b07b576547
Yash Raj <yashrj130@gmail.com>
2021-05-23 18:44:44 +0530

    Moving Card component to UI folder
    -also import changes made

--- src/components/ExpenseItems/ExpenseItem/ExpenseItem.js
+++ src/components/ExpenseItems/ExpenseItem/ExpenseItem.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import classes from './ExpenseItem.module.css';
 import ExpenseDate from './ExpenseDate';
-import Card from '../../Card/Card';
+import Card from '../../UI/Card/Card';
 
 const ExpenseItem = props => {
 
